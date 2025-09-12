04c2553470de9ccda77d617506f8e454d4e585fb
Tomer Schwartz <40770296+TomerSchDev@users.noreply.github.com>
2025-09-12 09:34:08 +0300

    added ReportingServiceTests.cs

---
+++ TestsLibrary/ReportingServiceTests.cs
@@ -0,0 +1,49 @@
+﻿using LibrarySystemModels.Models;
+using LibrarySystemModels.Services;
+using LibrarySystemModels.Helpers;
+using Xunit;
+
+namespace TestsLibrary
+{
+    public class ReportingServiceTests : IDisposable
+    {
+        private readonly string _dbTestPath;
+
+        public ReportingServiceTests()
+        {
+            _dbTestPath = Path.Combine("Resources", $"test_report_{Guid.NewGuid()}.sqlite");
+            DataBaseService.SetModes(false, true);
+            DataBaseService.Init(_dbTestPath);
+            AuthService.LoginAsync(FlowSide.Client, "Admin", "admin123").Wait();
+        }
+
+        public void Dispose()
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            // if (File.Exists(_dbTestPath)) File.Delete(_dbTestPath);
+        }
+
+        [Fact]
+        public async Task CanAddAndGetReport()
+        {
+           
+            await ReportingService.ReportEventAsync(FlowSide.Client,SeverityLevel.LOW,"Test report");
+            var getResult = await ReportingService.GetReportsAsync(FlowSide.Client);
+            Assert.True(getResult.ActionResult);
+            Assert.Contains(getResult.Data, r => r.ReportMessage == "Test report");
+        }
+
+        [Fact]
+        public async Task CanDeleteReport()
+        {
+            const string reportMessage = "For delete";
+           
+            await ReportingService.ReportEventAsync(FlowSide.Client,SeverityLevel.LOW,reportMessage);
+            var allReports = await ReportingService.GetReportsAsync(FlowSide.Client);
+            var rep = allReports.Data.First(r => r.ReportMessage == reportMessage);
+            var delResult = await ReportingService.DeleteReportAsync(FlowSide.Client, rep.ReportID);
+            Assert.True(delResult.ActionResult);
+        }
+    }
+}
